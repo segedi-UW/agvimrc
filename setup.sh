872310c49bd917a516a71702d375b708c01bbecd
Anthony Segedi <aj.segedi@gmail.com>
2022-06-15 15:57:45 -0500

    Symbolic link error fix

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # symbolically link vim directories so they are were vim expects them to be
 #      target : dest
-ln -s .vimrc ../.vimrc
-ln -s .vim ../.vim
-ln -s ./.vim/colorschemes/colors ../.vim/colors
+ln -sr .vimrc ..
+ln -sr .vim ..
+ln -sr ./.vim/colorschemes/colors ../.vim/colors
 # install all the git submodules (plugins). Alternatively install the ones you want individually (see GitHub)
 git submodule update --init
